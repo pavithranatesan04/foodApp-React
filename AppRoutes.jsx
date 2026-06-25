1c99f0be31fd8b40c4969520286c6a1230f0b3ef
Pavithra Natesan <pavithranatesan0412@gmail.com>
2026-06-25 11:45:36 +0530

    Add files via upload

---
+++ AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Offers from "../pages/Offers";
+import Restaurants from "../pages/Restaurants";
+import TrackOrder from "../pages/TrackOrder";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/track-order" element={<TrackOrder />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
